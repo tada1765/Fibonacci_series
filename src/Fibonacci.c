efc05e07ce9dbe4ba6c92d65fb9416066641c8cf
tada1765 <www.tada1765@gmail.com>
2018-06-06 12:38:11 +0800

    trying to code!

--- src/Fibonacci.c
+++ src/Fibonacci.c
@@ -1,1 +1,26 @@
 #include "Fibonacci.h"
+#include <stdio.h>
+
+//char mallocFibonacci_Series
+char* Fibonacci_main(int num)
+{
+	char Fibonacci_Series[20];
+    int t1 = 0, t2 = 1, nextTerm = 0;
+
+    //printf("Enter a number: ");
+    //scanf("%d", &num);
+
+    printf("Fibonacci Series: ");
+
+    for (int i = 1; i <= num; ++i)
+    {
+        printf("%d, ", t1);
+        nextTerm = t1 + t2;
+        t1 = t2;
+        t2 = nextTerm;
+		Fibonacci_Series[i-1] = t1; 
+    }
+	
+	
+    return Fibonacci_Series;
+}
--- test/test_Fibonacci.c
+++ test/test_Fibonacci.c
@@ -11,5 +11,16 @@ void tearDown(void)
 
 void test_Fibonacci_NeedToImplement(void)
 {
-    TEST_IGNORE_MESSAGE("Need to Implement Fibonacci");
+	TEST_IGNORE_MESSAGE("Need to Implement Fibonacci");
 }
+
+void test_Fibonacci_read_only_3rd_line_from_file_expect_true(void)
+  { 
+	char* data_str[] ={"0","1","1","2","3","5","8"};
+    char* Fibonacci;
+	int num = 4;
+	
+    Fibonacci = Fibonacci_main(num);
+    
+    TEST_ASSERT_EQUAL_STRING(data_str[1], Fibonacci);//the 3rd line from file
+  } 
